3705b0813b46045231906dd39eccc2a0d08e1844
Jeyakeethan <jeyakeethan@gmail.com>
2023-03-21 18:16:22 +0530

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,117 @@
+package org.example;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+public class Main {
+    public static void main(String[] args) {
+        /**
+         Outgoing Mail (SMTP) Server
+         requires TLS or SSL: smtp.gmail.com (use authentication)
+         Use Authentication: Yes
+         Port for SSL: 587
+         */
+        final String fromEmail = "jkeetha28@gmail.com"; //requires valid gmail id
+        final String password = "t6Y4dCO5w3rEvnmR"; // correct password for gmail id
+        final String toEmail = "nilusija@zerobeta.xyz"; // can be any email id
+
+        System.out.println("SSLEmail Start");
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp-relay.sendinblue.com"); //SMTP Host
+        props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
+        props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
+        props.put("mail.smtp.port", "587"); //SMTP Port
+
+        Authenticator auth = new Authenticator() {
+            //override the getPasswordAuthentication method
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, password);
+            }
+        };
+
+        Session session = Session.getDefaultInstance(props, auth);
+        System.out.println("Session created");
+        Main.sendEmail(session, toEmail,"SSLEmail Testing Subject", "SSLEmail Testing Body");
+    }
+
+
+    /**
+     * Utility method to send simple HTML email
+     * @param session
+     * @param toEmail
+     * @param subject
+     * @param body
+     */
+    public static void sendEmail(Session session, String toEmail, String subject, String body){
+        try
+        {
+            MimeMessage msg = new MimeMessage(session);
+            //set message headers
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress("no_reply@example.com", "NoReply-JD"));
+
+            msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setText(body, "UTF-8");
+
+            msg.setSentDate(new Date());
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+
+            // Create the message body part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Fill the message
+            messageBodyPart.setText(body);
+
+            // Create a multipart message for attachment
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Second part is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "D:\\Dev\\SimpleMailMvn\\src\\main\\java\\org\\example\\abc.csv";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            msg.setContent(multipart);
+
+
+            System.out.println("Message is ready");
+            Transport.send(msg);
+
+            System.out.println("Email Sent Successfully!!");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/MainOpen.java
@@ -0,0 +1,98 @@
+package org.example;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class MainOpen {
+    public static void main(String[] args) {
+        System.out.println("SimpleEmail Start");
+
+        String smtpHostServer = "smtp.freesmtpservers.com";
+        String emailID = "email_me@example.com";
+
+        Properties props = System.getProperties();
+
+        props.put("mail.smtp.host", smtpHostServer);
+
+        Session session = Session.getInstance(props, null);
+
+        MainOpen.sendEmail(session, emailID,"SimpleEmail Testing Subject", "SimpleEmail Testing Body");
+
+    }
+
+    /**
+     * Utility method to send simple HTML email
+     * @param session
+     * @param toEmail
+     * @param subject
+     * @param body
+     */
+    public static void sendEmail(Session session, String toEmail, String subject, String body){
+        try
+        {
+            MimeMessage msg = new MimeMessage(session);
+            //set message headers
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress("no_reply@example.com", "NoReply-JD"));
+
+            msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setText(body, "UTF-8");
+
+            msg.setSentDate(new Date());
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+
+            // Create the message body part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Fill the message
+            messageBodyPart.setText(body);
+
+            // Create a multipart message for attachment
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Second part is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "abc.csv";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            msg.setContent(multipart);
+
+
+            System.out.println("Message is ready");
+            Transport.send(msg);
+
+            System.out.println("EMail Sent Successfully!!");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
